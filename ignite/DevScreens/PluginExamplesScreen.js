c24b87caeeffeafa0aa3d95f412149da0156a4bf
Gant <GantMan@gmail.com>
2017-08-22 15:35:55 -0500

    begin multi-track

--- ignite/DevScreens/PluginExamplesScreen.js
+++ ignite/DevScreens/PluginExamplesScreen.js
@@ -8,6 +8,10 @@ import { Images } from './DevTheme'
 
 // Examples Render Engine
 import ExamplesRegistry from '../../App/Services/ExamplesRegistry'
+import SectionExample. from '../Examples/Containers/ignite-ir-boilerplate/examples/SectionExample.js.ejs'
+import GridExample. from '../Examples/Containers/ignite-ir-boilerplate/examples/GridExample.js.ejs'
+import RowExample. from '../Examples/Containers/ignite-ir-boilerplate/examples/RowExample.js.ejs'
+import RoundedButton from '../../App/Components/RoundedButton'
 import '../Examples/Components/MapsExample.js'
 import '../Examples/Components/animatableExample.js'
 import '../Examples/Components/i18nExample.js'
@@ -45,6 +49,18 @@ class PluginExamplesScreen extends React.Component {
 
           <View style={styles.screenButtons} />
 
+            <RoundedButton onPress={() => this.props.navigation.navigate('SectionExample.')}>
+              Sections Example
+            </RoundedButton>
+
+            <RoundedButton onPress={() => this.props.navigation.navigate('GridExample.')}>
+              Grid Example
+            </RoundedButton>
+
+            <RoundedButton onPress={() => this.props.navigation.navigate('RowExample.')}>
+              Row Example
+            </RoundedButton>
+
         </ScrollView>
       </View>
     )
@@ -52,6 +68,9 @@ class PluginExamplesScreen extends React.Component {
 }
 
 export default StackNavigator({
+  RowExample.: {screen: RowExample., navigationOptions: {header: {visible: true}}},
+  GridExample.: {screen: GridExample., navigationOptions: {header: {visible: true}}},
+  SectionExample.: {screen: SectionExample., navigationOptions: {header: {visible: true}}},
   PluginExamplesScreen: {screen: PluginExamplesScreen}
 }, {
   headerMode: 'screen',
