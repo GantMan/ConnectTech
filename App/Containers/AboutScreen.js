5ed06830ea9dc6f02dbb6998922922c582161c6d
Sia <siakaramalegos@gmail.com>
2017-09-15 14:54:12 -0500

    Removes sponsor tab and tab nav on Gen Info screen

--- App/Containers/AboutScreen.js
+++ App/Containers/AboutScreen.js
@@ -9,7 +9,6 @@ import {
 import BackgroundGradient from '../Components/BackgroundGradient'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Twitter from '../Components/Twitter'
-import Sponsors from '../Components/Sponsors'
 import LiveHelp from '../Components/LiveHelp'
 import { connect } from 'react-redux'
 import styles from './Styles/AboutScreenStyle'
@@ -30,69 +29,15 @@ class AboutScreen extends React.Component {
     )
   }
 
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      activeTab: 'liveHelp'
-    }
-  }
-
-  setActiveTab (tab) {
-    LayoutAnimation.configureNext({...LayoutAnimation.Presets.linear, duration: 250})
-    this.setState({activeTab: tab})
-  }
-
-  renderTabs () {
-    const { activeTab } = this.state
-    const liveHelpStyles = [
-      styles.tab, activeTab === 'liveHelp' && styles.activeTab
-    ]
-    const sponsorStyles = [
-      styles.tab, activeTab === 'sponsors' && styles.activeTab
-    ]
-    const liveHelpTextStyles = [
-      styles.tabText, activeTab === 'liveHelp' && styles.activeTabText
-    ]
-    const sponsorTextStyles = [
-      styles.tabText, activeTab === 'sponsors' && styles.activeTabText
-    ]
-
-    return (
-      <View style={styles.tabsContainer}>
-        <View style={styles.tabs}>
-          <TouchableOpacity
-            style={liveHelpStyles}
-            onPress={() => this.setActiveTab('liveHelp')}>
-            <Text style={liveHelpTextStyles}>
-              Live Help
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={sponsorStyles}
-            onPress={() => this.setActiveTab('sponsors')}>
-            <Text style={sponsorTextStyles}>
-              Sponsors
-            </Text>
-          </TouchableOpacity>
-        </View>
-        {this.renderTabsContent()}
-      </View>
-    )
-  }
-
-  renderTabsContent () {
-    const { activeTab } = this.state
-    return activeTab === 'liveHelp' ? <LiveHelp /> : <Sponsors />
-  }
-
   render () {
     return (
       <BackgroundGradient style={[styles.linearGradient, {flex: 1}]}>
         <ScrollView>
           <View style={styles.container}>
             <Twitter />
-            {this.renderTabs()}
+            <View style={styles.tabsContainer}>
+            <LiveHelp />
+          </View>
           </View>
         </ScrollView>
       </BackgroundGradient>
